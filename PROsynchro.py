6684bd7021b36fadf1a44a3d4d7683548b1d74c5
ProRoch <49270693+ProRoch@users.noreply.github.com>
2019-07-08 23:21:44 +0200

    Update PROsynchro.py

--- PROsynchro.py
+++ PROsynchro.py
@@ -2,7 +2,7 @@ import config as cfg
 import sys
 import types
 
-from PyQt5 import QtWidgets, QtCore, QtGui, Qt
+from PyQt5 import QtWidgets, QtCore, QtGui
 from mainLayoutGUI import Ui_MainWindow
 
 
@@ -30,7 +30,30 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
 
     def initWidgets(self):
-        pass
+
+        listMainFolders = ["usr/RTG/etc", "commisioning", "epc"]
+        listSubF_1 = ["rtg.ini","rtg_3cell.ini","rtg_4cell.ini"]
+        listSubF_2 = ["Commissioning.xml", "other_Commissioning.xml","5MHz_VoLTE.xml"]
+        listSubF_3 = ["epc.lua","epc_lmts.lua","My_epc.lua"]
+
+        parent = QtWidgets.QTreeWidgetItem(self.treeMainPC)
+        parent.setText(0, listMainFolders[0])
+        child = QtWidgets.QTreeWidgetItem(parent)
+        child.setText(0, listSubF_1[0])
+        child = QtWidgets.QTreeWidgetItem(parent)
+        child.setText(0, listSubF_1[1])
+        child = QtWidgets.QTreeWidgetItem(parent)
+        child.setText(0, listSubF_1[2])
+
+        parent = QtWidgets.QTreeWidgetItem(self.treeMainPC)
+        parent.setText(0, listMainFolders[1])
+        child = QtWidgets.QTreeWidgetItem(parent)
+        child.setText(0, listSubF_2[0])
+        child = QtWidgets.QTreeWidgetItem(parent)
+        child.setText(0, listSubF_2[1])
+        child = QtWidgets.QTreeWidgetItem(parent)
+        child.setText(0, listSubF_2[2])
+
 
         # Initialize button colours.
 
