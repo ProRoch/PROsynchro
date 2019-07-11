a96c5de59a162caa9c577a4c65b50005db8b6702
Pawel Rochumski <pawel.rochumski@nokia.com>
2019-07-11 15:43:52 +0200

    add readding local folder

--- PROsynchro.py
+++ PROsynchro.py
@@ -1,5 +1,5 @@
 import config as cfg
-import sys
+import sys, os, paramiko
 import types
 
 from PyQt5 import QtWidgets, QtCore, QtGui
@@ -21,6 +21,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
         self.setupUi(self)
         self.initWidgets()
+        self.lblWorking.hide()
         self.show()
 
         print("==================")
@@ -30,12 +31,16 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
 
     def initWidgets(self):
+        self.btnExit.clicked.connect(self.btn_exit)
+        self.btnExecuteAction.clicked.connect(self.btnExeActionClicked)
+        self.btnView.clicked.connect(self.btnRefreshView)
 
         listMainFolders = ["usr/RTG/etc", "commisioning", "epc"]
         listSubF_1 = ["rtg.ini","rtg_3cell.ini","rtg_4cell.ini"]
         listSubF_2 = ["Commissioning.xml", "other_Commissioning.xml","5MHz_VoLTE.xml"]
         listSubF_3 = ["epc.lua","epc_lmts.lua","My_epc.lua"]
 
+        """
         parent = QtWidgets.QTreeWidgetItem(self.treeMainPC)
         parent.setText(0, listMainFolders[0])
         child = QtWidgets.QTreeWidgetItem(parent)
@@ -53,13 +58,151 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         child.setText(0, listSubF_2[1])
         child = QtWidgets.QTreeWidgetItem(parent)
         child.setText(0, listSubF_2[2])
+        """
+
+        self.btnRefreshView()
 
 
         # Initialize button colours.
 
 
+    def btnExeActionClicked(self):
+        print("btn btnExeActionClicked clicked.")
+        self.lblWorking.hide()
+        enteredOption = 0
+        for i in range(1,7):
+            buttonName = self.findChild(QtWidgets.QRadioButton, "radioButton_" + str(i))
+            if buttonName.isChecked():
+                enteredOption = i
+        print("Action set for {}".format(enteredOption))
+
+        print("number of defined folder: {}".format(len(cfg.sync_folder)))
+        remoteHost = cfg.host_PAF
+        remoteHost = cfg.host_WS
+        """
+        "  -1-  copy file     Local    <<<-to---    WS"
+        "  -2-  copy file     Local    <<<-to---  ------  <<<-to---   PAF"
+        "  -3-  copy file     Local    ----to->>>   WS"
+        "  -4-  copy file     Local    ----to->>> ------  ----to->>>  PAF"
+        "  -5-  copy file     Local    ----to->>> - WS -  ----to->>>  PAF"
+        "  -9-  exit"
+        """
+        if enteredOption == 1 :     #"  -1-  copy file     Local    <<<-to---   WS"
+            self.copyFiles("WS","ToLocal")
+        elif enteredOption == 2 :     #"  -2-  copy file     Local    <<<-to---          <<<-to---   PAF"
+            self.copyFiles("PAF","ToLocal")
+        elif enteredOption == 3 :     #"  -3-  copy file     Local    ----to->>>  WS"
+            self.copyFiles("WS","ToRemote")
+        elif enteredOption == 4 :     #"  -4-  copy file     Local    ----to->>>        ----to->>>  PAF"
+            self.copyFiles("PAF","ToRemote")
+        elif enteredOption == 5 :     #"  -5-  copy file     Local    ----to->>>  WS     ----to->>>  PAF"
+            self.copyFiles("WS","ToRemote")
+            self.copyFiles("PAF","ToRemote")
+        elif enteredOption == 9 :
+            print("Have a nice day...")
+            quit()
+        self.lblWorking.show()
+
+    def copyFiles(self, remoteHost, cpyDirect):
+        for i in range(len(cfg.sync_folder)):
+            remotePC = cfg.host_WS if remoteHost == "WS" else cfg.host_PAF
+            self.getFileToLocal(cfg.sync_folder[i]["dir_local"],
+                              cfg.sync_folder[i]["dir_WS"] if remoteHost == "WS" else cfg.sync_folder[i]["dir_PAF"],
+                              remotePC["addr"],
+                              remotePC["user"],
+                              remotePC["pass"] if remotePC["pkey"] == False else remotePC["pkey"],
+                              cpyDirect)
+
+    def getFileToLocal(self, localDir, remoteDir, remoteAddr, remoteUsr, remotePass=False, cpyToRemote="ToLocal"):
+        localDir_full = os.getcwd() + "\\" + cfg.localFolderName + "\\" + localDir
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh_client.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
+        try:
+            k = paramiko.RSAKey.from_private_key_file(remotePass)
+            ssh_client.connect(hostname=remoteAddr, username=remoteUsr, pkey=k)
+        except:
+            ssh_client.connect(hostname=remoteAddr, username=remoteUsr, password=remotePass)
+        sftp = ssh_client.open_sftp()
+
+        item_list = []
+        if cpyToRemote == "ToRemote":
+            for (dirpath, dirnames, filenames) in os.walk(localDir_full):
+                for file in filenames:
+                    # item_list.append(os.path.join(dirpath, file))
+                    item_list.append(file)
+        else:
+            item_list = sftp.listdir(remoteDir)
+        print("----------list files=====:{}====================".format(item_list))
+
+        if not os.path.isdir(os.getcwd() + "\\" + cfg.localFolderName):
+            os.makedirs(os.getcwd() + "\\" + cfg.localFolderName)
+        if not os.path.isdir(localDir_full):
+            os.makedirs(localDir_full)
+
+        for file in item_list:
+            try:
+                localpath = localDir_full + "\\" + file
+                remotepath = remoteDir + "/" + file
+                if cpyToRemote == "ToLocal":
+                    sftp.get(remotepath, localpath)
+                elif cpyToRemote == "ToRemote":
+                    sftp.put(localpath, remotepath)
+                # print("---------------------------------")
+                # print("Local path: {}".format(localpath))
+                # print("Remote path: {}".format(remotepath))
+                ###sftp.put(localpath, remotepath)
+            except Exception as inst:
+                print("Some error in copy file:{}".format(remotepath))
+        sftp.close()
+        ssh_client.close()
 
 
+    def btnRefreshView(self):
+        self.lblWorking.hide()
+
+        i=0
+        for workingFolder in cfg.sync_folder:
+            treeLocal = QtWidgets.QTreeWidgetItem(self.treeMainPC)
+            treeWS = QtWidgets.QTreeWidgetItem(self.treeMainWS)
+            treePAF = QtWidgets.QTreeWidgetItem(self.treeMainPAF)
+
+            treeLocal.setText(0, workingFolder["name"])
+            treeWS.setText(0, workingFolder["name"])
+            treePAF.setText(0, workingFolder["name"])
+            #workingFolder["dir_local"]
+            #workingFolder["dir_WS"]
+            #workingFolder["dir_PAF"]
+            #workingFolder["name"]
+
+            item_list = []
+            #for Local
+            localDir_full = os.getcwd() + "\\" + cfg.localFolderName + "\\" + workingFolder["dir_local"]
+            for (dirpath, dirnames, filenames) in os.walk(localDir_full):
+                for file in filenames:
+                    # item_list.append(os.path.join(dirpath, file))
+                    item_list.append(file)
+                    child = QtWidgets.QTreeWidgetItem(treeLocal)
+                    child.setText(0, file)
+
+            """
+            #for WS
+            ssh_client = paramiko.SSHClient()
+            ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh_client.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
+            try:
+                ssh_client.connect(hostname=cfg.host_WS["addr"], username=cfg.host_WS["user"], pkey= cfg.host_WS["pkey"])
+            except:
+                ssh_client.connect(hostname=cfg.host_WS["addr"], username=cfg.host_WS["user"], pkey= cfg.host_WS["pass"])
+            sftp = ssh_client.open_sftp()
+            item_list = sftp.listdir(remoteDir)
+    
+            #for PAF
+            """
+
+    def btn_exit(self):
+        print("btn btnExit clicked.")
+        sys.exit()
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
--- config.py
+++ config.py
@@ -1,22 +1,45 @@
-#from PyQt5.QtWidgets import *
 
-uiButtonTable = []
-ledStripLine = []           # Object of Led strip LEDs
-myGrapicsScene = 1
-mySingleLedShape = 0 #- 0-Rect, 1-Circle
-myItemTabHandler = []
-#quantuty of LED in strip
-myLedInSingleRow = 30       #--== how may LED in one line of strip
-myLedRow = 15               #--== how many strip
-#Point size and position
-myLedWith = 20              #==-- in pix X size on screen
-myLedHight = 20             #==-- in pix Y size on screen
-myLedDeltaX = 30            #==-- space in pix for 1 Led + 1 break in X
-myLedDeltaY = 50            #==-- space in pix for 1 Led + 1 break in Y
-#Program parameter
-myProgMain = False          # ==-- handler for current program
-myLedPointerMain = 0
+localFolderName = "TL_80_RTG"
 
-myColorPrimary = 0x000000
-myColorBg = 0xFFFFFF
-myDeltaTime = 10
+host_WS = {"addr" : "ssp-ws-macro080.macro.krk-lab.nsn-rdnet.net",
+           "user" : "lmts",
+           "pkey" : False,
+           "pass" : "ssp123!!Macro"}
+
+host_PAF = {"addr": "belvedere04.krk-lab.nsn-rdnet.net",
+            "user": "rochumsk",
+            "pkey": r"d:\PROchumski\Python\my_RSAKey\for_win",
+            "pass": False}
+
+sync_folder = []
+##-----------------------------------------------------------------
+new_folder = {  "dir_local" : r"usr_xxx_etc",
+                "dir_WS"    : r"/usr/rtg/etc",
+                "dir_PAF"   : r"/home/rochumsk/auto/sstlib_scripts/PAF/CONF/RTG/PET/CONFIGURATION/FSMr3-FDD-080",
+                "name"      : "usr_xxx_etc"}
+sync_folder.append(new_folder)
+##-----------------------------------------------------------------
+
+new_folder = {  "dir_local" : "WTS_BIN",
+                "dir_WS"    : r"/home/lmts/WTS_BIN/configs",
+                "dir_PAF"   : r"/home/rochumsk/auto/sstlib_scripts/PAF/CONF/EPCSIM/PET/FSMr3-FDD-080",
+                "name"      : "wts_bin"}
+sync_folder.append(new_folder)
+##-----------------------------------------------------------------
+
+
+new_folder = {  "dir_local" : "comiss_SBTS19A",
+                "dir_WS"    : r"/home/lmts/Comissionings/SBTS19A",
+                "dir_PAF"   : r"/home/rochumsk/auto/sstlib_scripts/PAF/CONF/ENB/PET/COMMISSIONING/FSM/FSMr3-FDD-080/FL19A",
+                "name"      : "commissioning"}
+sync_folder.append(new_folder)
+##-----------------------------------------------------------------
+
+new_folder = {  "dir_local" : "testing",
+                "dir_WS"    : r"/home/lmts/PROchumski/TL80_for_testing",
+                "dir_PAF"   : r"/home/rochumsk/test",
+                "name"      : "for_test_script"}
+#sync_folder.append(new_folder)
+##-----------------------------------------------------------------
+
+
